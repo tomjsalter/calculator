1a5818512584dfd60c271d8f1fce6d254164550b
Tom Salter <tomjsalter@protonmail.com>
2022-11-15 22:23:25 +0000

    Add basic math functions

---
+++ script.js
@@ -0,0 +1,21 @@
+function add(a, b) {
+    let sum = 0;
+    return sum = a + b;
+}
+
+function subtract(a, b) {
+    let sum = 0;
+    return sum = a - b;
+}
+
+function multiply(a, b) {
+    let sum = 0;
+    return sum = a * b;
+}
+
+function divide(a, b) {
+    let sum = 0;
+    return sum = a / b;
+}
+
+// operate function
