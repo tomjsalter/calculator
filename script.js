18c4e865030171cf4cc1b6defd0945f5a518c226
Tom Salter <tomjsalter@protonmail.com>
2022-11-15 23:00:20 +0000

    Reduce repetition in basic math functions

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
+let sum = 0;
 function add(a, b) {
-  let sum = 0;
   return (sum = a + b);
 }
 
 function subtract(a, b) {
-  let sum = 0;
   return (sum = a - b);
 }
 
 function multiply(a, b) {
-  let sum = 0;
   return (sum = a * b);
 }
 
 function divide(a, b) {
-  let sum = 0;
   return (sum = a / b);
 }
 
 function operate(operator, a, b) {
   switch (operator) {
     case operator === "add":
-        add(a, b);
+      add(a, b);
       break;
     case operator === "subtract":
       subtract(a, b);
