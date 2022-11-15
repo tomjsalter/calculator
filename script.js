fea6c8934daa2abdfca56013d461480cd4ee95c0
Tom Salter <tomjsalter@protonmail.com>
2022-11-15 22:57:06 +0000

    Add operator function

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 function add(a, b) {
-    let sum = 0;
-    return sum = a + b;
+  let sum = 0;
+  return (sum = a + b);
 }
 
 function subtract(a, b) {
-    let sum = 0;
-    return sum = a - b;
+  let sum = 0;
+  return (sum = a - b);
 }
 
 function multiply(a, b) {
-    let sum = 0;
-    return sum = a * b;
+  let sum = 0;
+  return (sum = a * b);
 }
 
 function divide(a, b) {
-    let sum = 0;
-    return sum = a / b;
+  let sum = 0;
+  return (sum = a / b);
 }
 
-// operate function
+function operate(operator, a, b) {
+  switch (operator) {
+    case operator === "add":
+        add(a, b);
+      break;
+    case operator === "subtract":
+      subtract(a, b);
+      break;
+    case operator === "multiply":
+      multiply(a, b);
+      break;
+    case operator === "divide":
+      divide(a, b);
+      break;
+  }
+}
