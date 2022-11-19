f433b20e23eab7e9f915e56aea85045151cb59cc
Tom Salter <tomjsalter@protonmail.com>
2022-11-19 20:05:51 +0000

    Add logic to equals button

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-const numBtns = document.querySelectorAll(".number");
-const operateBtns = document.querySelectorAll(".operator");
+const numButtons = document.querySelectorAll(".number");
+const operateButtons = document.querySelectorAll(".operator");
+const equalsButton = document.getElementById("equals");
 let calcDisplay = document.getElementById("display");
 let sum = 0;
+let num1 = 0;
+let num2 = 0;
 let displayValue = 0;
+let operator;
 
 function add(num1, num2) {
   return (sum = num1 + num2);
@@ -22,7 +26,7 @@ function divide(num1, num2) {
 
 function operate(operator, num1, num2) {
   switch (operator) {
-    case operator === "add":
+    case operator === "addition":
       add(num1, num2);
       break;
     case operator === "subtract":
@@ -37,15 +41,21 @@ function operate(operator, num1, num2) {
   }
 }
 
-numBtns.forEach((button) => {
+numButtons.forEach((button) => {
   button.addEventListener('click', () => {
    calcDisplay.textContent += +button.textContent;
    displayValue = +calcDisplay.textContent;
+   return displayValue;
   });
 });
 
-operateBtns.forEach((button) => {
+operateButtons.forEach((button) => {
   button.addEventListener('click', () => {
-    
+    num1 = displayValue;
+    operator = button.id;
   });
+});
+
+equalsButton.addEventListener('click', () => {
+  operate(operator, num1, num2);
 });
