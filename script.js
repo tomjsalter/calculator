2a53eb5d9de7f266fd4a624b6780bf351e19d570
Tom Salter <tomjsalter@protonmail.com>
2022-11-20 13:36:48 +0000

    Begin to rework calculator logic

--- script.js
+++ script.js
@@ -1,82 +1,30 @@
-const numButtons = document.querySelectorAll(".number");
-const operateButtons = document.querySelectorAll(".operator");
-const equalsButton = document.getElementById("equals");
-let calcDisplay = document.getElementById("display");
+const numberButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
+const resetButton = document.getElementById('reset');
+const equalsButton = document.getElementById('equals');
+const calcuatorDisplay = document.getElementById('display');
 let sum = 0;
 let num1 = 0;
 let num2 = 0;
-let displayValue = 0;
-let operator;
+let operator = undefined;
 
 function add(num1, num2) {
   sum = num1 + num2;
-  calcDisplay = sum;
-  return sum;
 }
 
-function subtract(num1, b) {
+function subtract(num1, num2) {
   sum = num1 - num2;
-  calcDisplay = sum;
-  return sum;
 }
 
 function multiply(num1, num2) {
   sum = num1 * num2;
-  calcDisplay = sum;
-  return sum;
 }
 
 function divide(num1, num2) {
   sum = num1 / num2;
-  calcDisplay = sum;
-  return sum;
 }
 
-function operate(operator, num1, num2) {
-  switch (operator) {
-    case operator === "addition":
-      add(num1, num2);
-      break;
-    case operator === "subtract":
-      subtract(num1, num2);
-      break;
-    case operator === "multiply":
-      multiply(num1, num2);
-      break;
-    case operator === "divide":
-      divide(num1, num2);
-      break;
-  }
+function operate() {
+  operator = button.id;
+  
 }
-
-numButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    calcDisplay.textContent += +button.textContent;
-    displayValue = +calcDisplay.textContent;
-    return displayValue;
-  });
-});
-
-operateButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    num1 = displayValue;
-    operator = button.id;
-    numberTwo();
-  });
-});
-
-function numberTwo() {
-  numButtons.forEach((button) => {
-    if (num2 !== undefined) {
-      calcDisplay.removeChild.textContent;
-    }
-    button.addEventListener("click", () => {
-      num2 = +calcDisplay.textContent;
-      return num2;
-    });
-  });
-}
-
-equalsButton.addEventListener("click", () => {
-  operate();
-});
